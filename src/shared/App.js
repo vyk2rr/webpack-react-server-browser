075fae09aba9580d6b9201ed0c26bed8b0835db0
Victor De la Rocha <vyk2rr@gmail.com>
2017-07-29 23:15:49 -0500

    init

---
+++ src/shared/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      clicked: false
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    console.log('this is a test');
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
+  render() {
+    const hiddenPartUntilClick = this.state.clicked ? (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    ) : null;
+
+    return (
+      <div>
+        <button onClick={this.onClick}>Click here to toggle</button>
+        {hiddenPartUntilClick}
+      </div>
+    );
+  }
+}
+
+export default App;
